efe92e79c460293729d39332bf680b4d8e1be4f0
vishal021 <vishal.lambton@gmail.com>
2018-07-12 16:19:43 -0400

    Add post todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    password: {
+        type: String
+    }
+});
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,64 +1,25 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');//take your json and convert it into an object
 
-mongoose.Promise = global.Promise;// use global JS promise
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+app.use(bodyParser.json());//middleware bodyParser.json() return function
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    }); 
 });
 
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    password: {
-        type: String
-    }
-});
-
-// var newTodo = new Todo({
-//     text: 'cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Save todo', doc);
-// }, (e) => {
-//     console.log('Unable to save Todo', e);
-// });
-
-//mongoose casts numbers and boolean into a string 
-// var otherTodo = new Todo({
-//     text: 'something to do'
-// });
-
-// otherTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save Todo', e);
-// });
-
-var newUser = new User({
-    email: 'abc@gmail.com'
-});
-
-newUser.save().then((doc) => {
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-    console.log('Unable to insert new user', e);
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
