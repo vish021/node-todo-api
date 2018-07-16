0f9d12b1f126722b38a6186c0e40828f99607d59
vishal021 <vishal.lambton@gmail.com>
2018-07-16 16:35:14 -0400

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const {ObjectId} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');//take your json and convert it into an object
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');//take your json and convert it into an object
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -63,6 +64,32 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);//we don't want users to update anything they choose
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
