7e103cb9246ed22f54cd50030d5202142d6dbc46
vishal <vishal.joshi.021@gmail.com>
2018-07-18 21:38:50 -0400

    Add post /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');// all of the methods in this library supports only callbacks not promises
 
 //NOTE: mongoose runs a middleware; lets you run certain events  before or after
 var UserSchema = new mongoose.Schema({
@@ -74,6 +74,26 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+}
 
 UserSchema.pre('save', function(next) {
     var user = this;
