0b008dbd49d7d3c307b0e84598d8d14ee27d44e8
vishal021 <vishal.lambton@gmail.com>
2018-08-02 14:57:23 -0400

    converting code into async-await

--- server/server.js
+++ server/server.js
@@ -13,129 +13,125 @@ var app = express();
 
 app.use(bodyParser.json());//middleware bodyParser.json() return function
 
-app.post('/todos', authenticate, (req, res) => {
-    var todo = new Todo({
-        text: req.body.text,
-        _creator: req.user._id
-    });
+app.post('/todos', authenticate, async (req, res) => {
+    try {
+        var todo = new Todo({
+            text: req.body.text,
+            _creator: req.user._id
+        });
 
-    todo.save().then((doc) => {
+        const doc = await todo.save();
         res.send(doc);
-    }, (e) => {
+    } catch (e) {
         res.status(400).send(e);
-    }); 
+    }
 });
 
-app.get('/todos', authenticate, (req, res) => {
-    Todo.find({ _creator: req.user._id}).then((todos) => {
+app.get('/todos', authenticate, async (req, res) => {
+    try {
+        const todos = await Todo.find({ _creator: req.user._id});
         res.send({todos});
-    }, (e) => {
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
-app.get('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
+app.get('/todos/:id', authenticate, async (req, res) => {
+    try {
+        var id = req.params.id;
+        if(!ObjectId.isValid(id)) {
+            return res.status(404).send();
+        }
 
-    if(!ObjectId.isValid(id)) {
-        return res.status(404).send();
+        const todo = await Todo.findOne({_id: id, _creator: req.user._id});
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    } catch (e) {
+        res.status(400).send();
     }
+});
 
-    Todo.findOne({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    try {
+        var id = req.params.id;
+        if(!ObjectId.isValid(id)) {
+            return res.status(404).send();
+        }
+
+        const todo =  await Todo.findOneAndRemove({_id: id, _creator: req.user._id});
         if(!todo) {
             return res.status(404).send();
         }
         res.send({todo});
-    }, (e) => {
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
+app.patch('/todos/:id', authenticate, async (req, res) => {
+    try {
+        var id = req.params.id;
+        var body = _.pick(req.body, ['text', 'completed']);//we don't want users to update anything they choose
 
-    if(!ObjectId.isValid(id)) {
-        return res.status(404).send();
-    }
+        if(!ObjectId.isValid(id)) {
+            return res.status(404).send();
+        }
 
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+        if(_.isBoolean(body.completed) && body.completed) {
+            body.completedAt = new Date().getTime();
+        } else {
+            body.completed = false;
+            body.completedAt = null;
+        }
+
+        const todo = await Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true});
         if(!todo) {
             return res.status(404).send();
         }
         res.send({todo});
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send();
-    });
-});
-
-app.patch('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
-    var body = _.pick(req.body, ['text', 'completed']);//we don't want users to update anything they choose
-
-    if(!ObjectId.isValid(id)) {
-        return res.status(404).send();
     }
-
-    if(_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime();
-    } else {
-        body.completed = false;
-        body.completedAt = null;
-    }
-
-    Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
-        if(!todo) {
-            return res.status(404).send();
-        }
-        res.send({todo});
-    }).catch((e) => {
-        res.status(400).send();
-    });
 
 });
 
 //this route should be public so that user would signup and get back the token
-app.post('/users', (req, res) => {
-    var user = new User(_.pick(req.body, ['email', 'password']));
-
-    user.save().then(() => {
-      return user.generateAuthToken();
-    }).then((token) => {
-        //x- is meant for custom header
-        res.header('x-auth', token).send(user);
-    }).catch((e) => {
+app.post('/users', async (req, res) => {
+    try {
+        var user = new User(_.pick(req.body, ['email', 'password']));
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);//x- is meant for custom header
+    } catch (e) {
         res.status(400).send(e);
-    }); 
+    }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch((e) => {
+app.post('/users/login', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.listen(process.env.PORT, () => {
